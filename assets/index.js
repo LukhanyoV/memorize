60cc8792185f176d44628ce0e19f8bcbadd62c13
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2023-02-11 13:39:19 +0200

    Can now generate and display sequence

---
+++ assets/index.js
@@ -0,0 +1,50 @@
+const {createApp} = Vue
+
+const app = {
+	data() {
+		return {
+			appName: 'Memorize',
+			rules: [
+				'Memorize and replay the sequence to advance to next level',
+				'Sequence has to replayed in the order you saw it in',
+				'The number of square that make the sequence will increase'
+			],
+			currentLevel: 1,
+			gameBoard: [],
+			currentBoard: [],
+			currentSequence: [],
+			playerSequence: [],
+			timer: null
+
+		}
+	},
+	methods: {
+		randomSquare() {
+			let rand = this.shuffle(this.gameBoard)
+			while(rand === this.currentSequence[this.currentSequence.length-1]) rand = this.shuffle(this.gameBoard)
+			this.currentSequence.push(rand)
+			this.currentBoard = this.gameBoard.map((tile, index) => rand === index ? {...tile, color: 'green'} : {...tile})
+		},
+		playSequence() {
+			this.timer = setInterval(() => {
+				if(this.currentSequence.length >= this.currentLevel + 4){
+					clearInterval(this.timer)
+				} else {
+					this.randomSquare()
+				}
+			}, 900);
+		},
+		shuffle(arr) {
+			return !Array.isArray(arr) ? 0 : Math.floor(Math.random()*arr.length)
+		}
+	},
+	mounted() {
+		const board = new Array(9).fill({})
+		this.gameBoard = board.map((tile, index) => {
+			return {...tile, id: index}
+		})
+		this.currentBoard = this.gameBoard
+	}
+}
+
+createApp(app).mount('#app')
